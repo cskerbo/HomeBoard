1039a2fc3230d34fba1a5aedf5d5ed86a6400dc1
cskerbo <chris.kerbo@outlook.com>
2020-03-05 02:21:35 -0800

    Adding forecast to weather API

--- app/helpers/weather_widgets_helper.rb
+++ app/helpers/weather_widgets_helper.rb
@@ -2,7 +2,7 @@ module WeatherWidgetsHelper
 
   def create_weather_widget(id)
     @home = Home.find(id)
-    weather_data = generate_weather_data(@home.zip_code)
+    weather_data = generate_forecast_today(@home.zip_code)
     @weather_widget = WeatherWidget.create(weather_data)
     @weather_widget.home_id = @home.id
     @weather_widget.save!
@@ -10,7 +10,7 @@ module WeatherWidgetsHelper
     @home.save
 end
 
-  def generate_weather_data(zip_code)
+  def generate_forecast_today(zip_code)
     weather_data = Hash.new
     path = "https://api.openweathermap.org/data/2.5/weather?zip=" + "#{zip_code}" + ",us&units=imperial&appid=0015579483ea0225b3814537df74bf36"
     uri = URI.parse(path)
@@ -29,10 +29,34 @@ end
     return weather_data
   end
 
+  def forecast_data(zip_code)
+    path = "https://api.openweathermap.org/data/2.5/forecast?zip=" + "#{zip_code}" + ",us&units=imperial&appid=0015579483ea0225b3814537df74bf36"
+    uri = URI.parse(path)
+    response = Net::HTTP.get(uri)
+    forecast = JSON.parse(response)
+    forecast
+  end
+
+  def update_forecast(zip_code)
+    forecast_hash = Hash.new
+    forecast = forecast_data(zip_code)
+    forecast['list'].each do |day|
+      today = day['dt']
+      final_today = Time.at(today)
+      puts final_today.strftime('%-m/%-d %I:%M %P')
+    end
+  end
+
+
+
+  end
+
+
+
   def update_weather_widget(id)
     @weather_widget = WeatherWidget.find(id)
     @home = Home.find(@weather_widget.home_id)
-    weather_data = generate_weather_data(@home.zip_code)
+    weather_data = generate_forecast_today(@home.zip_code)
     @weather_widget.update(weather_data)
     @weather_widget.updated_at = Time.now
     @weather_widget.save!
